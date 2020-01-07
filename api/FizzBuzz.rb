be1209fdad93d9b9f5f3e0828df48b7b8ef45644
k2works <kakimomokuri@gmail.com>
2020-01-07 18:48:07 +0900

    refactor: ポリモーフィズムによる条件記述の置き換え

--- api/FizzBuzz.rb
+++ api/FizzBuzz.rb
@@ -126,14 +126,10 @@ class FizzBuzzTest < Minitest::Test
     end
 
     describe 'それ以外のタイプの場合' do
-      def setup
-        @fizzbuzz = FizzBuzz.create(4)
-      end
-
       def test_例外を返す
         e =
           assert_raises RuntimeError do
-            @fizzbuzz.generate(1)
+            FizzBuzz.create(4)
           end
 
         assert_equal '該当するタイプは存在しません', e.message
