fb795e5f5c5cecf4fc854509464e5267a04ffde3
k2works <kakimomokuri@gmail.com>
2020-01-07 17:36:12 +0900

    test: タイプ1の場合

--- api/FizzBuzz.rb
+++ api/FizzBuzz.rb
@@ -70,13 +70,16 @@ class FizzBuzz
   MAX_NUMBER = 100
 
   def self.generate(number, type = 1)
-    is_fizz = number.modulo(3).zero?
-    is_buzz = number.modulo(5).zero?
+    case type
+    when 1
+      is_fizz = number.modulo(3).zero?
+      is_buzz = number.modulo(5).zero?
 
-    return 'FizzBuzz' if is_fizz && is_buzz
-    return 'Fizz' if is_fizz
-    return 'Buzz' if is_buzz
-    number.to_s
+      return 'FizzBuzz' if is_fizz && is_buzz
+      return 'Fizz' if is_fizz
+      return 'Buzz' if is_buzz
+      number.to_s
+    end
   end
 
   def self.generate_list
