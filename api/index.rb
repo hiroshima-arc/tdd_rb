ac0e0f4f78d52b4d5b40cb7c32bbe542b7324fd2
k2works <kakimomokuri@gmail.com>
2020-01-07 19:00:46 +0900

    fix: プリントする

--- api/index.rb
+++ api/index.rb
@@ -3,7 +3,8 @@ require_relative './FizzBuzz.rb'
 
 Handler =
   Proc.new do |req, res|
-    message = FizzBuzz.generate_list
+    fizzbuzz = FizzBuzz.new(1)
+    message = fizzbuzz.generate_list
     res.status = 200
     res['Content-Type'] = 'text/plain'
     res.body = Cowsay.say(message.to_s, 'cow')
