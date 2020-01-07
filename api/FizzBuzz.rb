f435d5aa6ab533e5e26e7276703210b6d9559fcc
k2works <kakimomokuri@gmail.com>
2020-01-07 15:22:18 +0900

    test: 1を渡したら文字列1を返す

--- api/FizzBuzz.rb
+++ api/FizzBuzz.rb
@@ -1,13 +1,16 @@
 require 'minitest/autorun'
 
 class FizzBuzzTest < Minitest::Test
-  def test_greeting
-    assert_equal 'Hello', FizzBuzz.greeting
+  def test_1を渡したら文字列1を返す
+    # 前準備
+    # 実行
+    # 検証
+    assert_equal '1', FizzBuzz.generate(1)
   end
 end
 
 class FizzBuzz
-  def self.greeting
-    'Hello'
+  def self.generate(n)
+    '1'
   end
 end
