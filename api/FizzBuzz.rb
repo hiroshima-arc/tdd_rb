50fede7acf898fba9533956320134b17c37988ca
k2works <kakimomokuri@gmail.com>
2020-01-07 18:50:39 +0900

    refactor: サブクラスによるタイプコードの置き換え

--- api/FizzBuzz.rb
+++ api/FizzBuzz.rb
@@ -143,7 +143,7 @@ class FizzBuzz
   attr_reader :list
 
   def initialize(type)
-    @type = type
+    @type = FizzBuzz.create(type)
   end
 
   def self.create(type)
@@ -159,28 +159,8 @@ class FizzBuzz
     end
   end
 
-  def generate(number)
-    is_fizz = number.modulo(3).zero?
-    is_buzz = number.modulo(5).zero?
-
-    case @type
-    when 1
-      return 'FizzBuzz' if is_fizz && is_buzz
-      return 'Fizz' if is_fizz
-      return 'Buzz' if is_buzz
-      number.to_s
-    when 2
-      number.to_s
-    when 3
-      return 'FizzBuzz' if is_fizz && is_buzz
-      number.to_s
-    else
-      raise '該当するタイプは存在しません'
-    end
-  end
-
   def generate_list
-    @list = (1..MAX_NUMBER).map { |i| generate(i) }
+    @list = (1..MAX_NUMBER).map { |i| @type.generate(i) }
   end
 end
 
