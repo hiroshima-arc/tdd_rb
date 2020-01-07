517b884ac5f097fcb79abaa430229486d91a1fcd
k2works <kakimomokuri@gmail.com>
2020-01-07 16:54:15 +0900

    feat: プリントする

--- api/index.rb
+++ api/index.rb
@@ -1,8 +1,10 @@
 require 'cowsay'
+require_relative './FizzBuzz.rb'
 
 Handler =
   Proc.new do |req, res|
+    message = FizzBuzz.generate_list
     res.status = 200
     res['Content-Type'] = 'text/plain'
-    res.body = Cowsay.say('hello world', 'cow')
+    res.body = Cowsay.say(message.to_s, 'cow')
   end
