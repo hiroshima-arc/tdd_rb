4a4fc78f8d381e7a500d5a70f1503a969a58601a
k2works <kakimomokuri@gmail.com>
2020-01-10 17:04:16 +0900

    feat: 本番環境と開発環境で表示を切り替える

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@ import FizzBuzzView from './presentation/FizzBuzzView'
 
 const view = new FizzBuzzView();
 view.render();
+
+if (process.env.NODE_ENV === 'production') {
+  if (document.getElementById('dev')) {
+    document.getElementById('dev').style.display = 'none'
+  }
+  if (document.getElementById('app-dev')) {
+    document.getElementById('app-dev').style.display = 'none'
+  }
+}
