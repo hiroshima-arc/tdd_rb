af954129b95ce0b083f1cc9bcbf0324b4c91fea9
k2works <kakimomokuri@gmail.com>
2020-01-10 14:35:45 +0900

    test: E2Eテスト

--- cypress/integration/fizz_buzz/fizz_buzz_page_spec.js
+++ cypress/integration/fizz_buzz/fizz_buzz_page_spec.js
@@ -6,6 +6,32 @@ describe("FizzBuzz Page", () => {
   });
 
   it("機能名が表示される", () => {
-    cy.get('#app > :nth-child(1) > :nth-child(1)').should("contain", "FizzBuzz");
+    cy.get("#app > :nth-child(1) > :nth-child(1)").should(
+      "contain",
+      "FizzBuzz"
+    );
+  });
+
+  describe("カウンター画面", () => {
+    beforeEach(() => {
+      cy.get("#tab-menu01").click();
+    });
+
+    it("初期値が表示される", () => {
+      cy.get(".display-1").should("contain", "FizzBuzz");
+    });
+
+    it("インクリメントすると値が変わる", () => {
+      cy.get('#increment').click();
+      cy.get('#increment').click();
+      cy.get('#increment').click();
+      cy.get(".display-1").should("contain", "Fizz");
+    });
+
+    it("デクリメントすると値が変わる", () => {
+      cy.get('#decrement').click();
+      cy.get('#decrement').click();
+      cy.get(".display-1").should("contain", "1");
+    });
   });
 });
