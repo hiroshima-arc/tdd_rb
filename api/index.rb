24b293baf59c8fe1cead52392045f650bbcb275b
k2works <kakimomokuri@gmail.com>
2020-01-11 13:18:07 +0900

    fix: Apiサンプル呼び出し

--- api/index.rb
+++ api/index.rb
@@ -1,12 +1,12 @@
 require 'cowsay'
-require_relative './fizz_buzz/application/fizz_buzz_list_command.rb'
+require_relative './fizz_buzz/application/service/fizz_buzz_list_command.rb'
 require_relative './fizz_buzz/domain/type/fizz_buzz_type.rb'
 require_relative './fizz_buzz/domain/type/fizz_buzz_type_01.rb'
 
 Handler =
   Proc.new do |req, res|
     fizzbuzz =
-      FizzBuzz::Application::FizzBuzzListCommand.new(
+      FizzBuzz::Application::Service::FizzBuzzListCommand.new(
         FizzBuzz::Domain::Type::FizzBuzzType.create(
           FizzBuzz::Domain::Type::FizzBuzzType::TYPE_01
         )
