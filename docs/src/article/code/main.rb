1580e7000cb8e68b49e9b08aac4bbcab3fe396bf
k2works <kakimomokuri@gmail.com>
2020-01-11 18:52:44 +0900

    docs: リファクタリングから始めるテスト駆動開発

--- docs/src/article/code/main.rb
+++ docs/src/article/code/main.rb
@@ -3,21 +3,39 @@ Minitest::Reporters.use!
 require 'minitest/autorun'
 
 class FizzBuzzTest < Minitest::Test
-  def setup
-    @fizzbuzz = FizzBuzz
-  end
+  describe 'FizzBuzz' do
+    def setup
+      @fizzbuzz = FizzBuzz
+    end
 
-  def test_1を渡したら文字列1を返す
-    assert_equal '1', FizzBuzz.generate(1)
-  end
+    describe '三の倍数の場合' do
+      def test_3を渡したら文字列Fizzを返す
+        assert_equal 'Fizz', @fizzbuzz.generate(3)
+      end
+    end
 
-  def test_2を渡したら文字列2を返す
-    assert_equal '2', FizzBuzz.generate(2)
+    describe '五の倍数の場合' do
+      def test_5を渡したら文字列Buzzを返す
+        assert_equal 'Buzz', @fizzbuzz.generate(5)
+      end
+    end
+
+    describe 'その他の場合' do
+      def test_1を渡したら文字列1を返す
+        assert_equal '1', @fizzbuzz.generate(1)
+      end
+    end
   end
 end
 
 class FizzBuzz
   def self.generate(number)
-    number.to_s
+    result = number.to_s
+    if number.modulo(3).zero?
+      result = 'Fizz'
+    elsif number.modulo(5).zero?
+      result = 'Buzz'
+    end
+    result
   end
 end
