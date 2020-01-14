3ed5dd8da24021d4b98f5abfd0aec9cad19dcba2
k2works <kakimomokuri@gmail.com>
2020-01-14 13:57:51 +0900

    docs: リファクタリングから始めるテスト駆動開発

--- docs/src/article/code/main.rb
+++ docs/src/article/code/main.rb
@@ -58,9 +58,89 @@ class FizzBuzzTest < Minitest::Test
       end
     end
   end
+
+  describe '配列や繰り返し処理を理解する' do
+    def test_繰り返し処理
+      $stdout = StringIO.new
+      [1, 2, 3].each { |i| p i * i }
+      output = $stdout.string
+
+      assert_equal "1\n" + "4\n" + "9\n", output
+    end
+
+    def test_特定の条件を満たす要素だけを配列に入れて返す
+      result = [1.1, 2, 3.3, 4].select(&:integer?)
+      assert_equal [2, 4], result
+    end
+
+    def test_特定の条件を満たす要素だけを配列に入れて返す
+      result = [1.1, 2, 3.3, 4].find_all(&:integer?)
+      assert_equal [2, 4], result
+    end
+
+    def test_特定の条件を満たさない要素だけを配列に入れて返す
+      result = [1.1, 2, 3.3, 4].reject(&:integer?)
+      assert_equal [1.1, 3.3], result
+    end
+
+    def test_新しい要素の配列を返す
+      result = %w[apple orange pineapple strawberry].map(&:size)
+      assert_equal [5, 6, 9, 10], result
+    end
+
+    def test_新しい要素の配列を返す
+      result = %w[apple orange pineapple strawberry].collect(&:size)
+      assert_equal [5, 6, 9, 10], result
+    end
+
+    def test_配列の中から条件に一致する要素を取得する
+      result = %w[apple orange pineapple strawberry].find(&:size)
+      assert_equal 'apple', result
+    end
+
+    def test_配列の中から条件に一致する要素を取得する
+      result = %w[apple orange pineapple strawberry].detect(&:size)
+      assert_equal 'apple', result
+    end
+
+    def test_指定した評価式で並び変えた配列を返す
+      assert_equal %w[1 10 13 2 3 4], %w[2 4 13 3 1 10].sort
+      assert_equal %w[1 2 3 4 10 13],
+                   %w[2 4 13 3 1 10].sort { |a, b| a.to_i <=> b.to_i }
+      assert_equal %w[13 10 4 3 2 1],
+                   %w[2 4 13 3 1 10].sort { |b, a| a.to_i <=> b.to_i }
+    end
+
+    def test_配列の中から、条件に一致する要素を取得する
+      result = %w[apple orange pineapple strawberry apricot].grep(/^a/)
+      assert_equal %w[apple apricot], result
+    end
+
+    def test_ブロック内の条件式が真である間までの要素を返す
+      result = [1, 2, 3, 4, 5, 6, 7, 8, 9].take_while { |item| item < 6 }
+      assert_equal [1, 2, 3, 4, 5], result
+    end
+
+    def test_ブロック内の条件式が真である以降の要素を返す
+      result = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].drop_while { |item| item < 6 }
+      assert_equal [6, 7, 8, 9, 10], result
+    end
+
+    def test_畳み込み演算を行う
+      result = [1, 2, 3, 4, 5].inject(0) { |total, n| total + n }
+      assert_equal 15, result
+    end
+
+    def test_畳み込み演算を行う
+      result = [1, 2, 3, 4, 5].reduce { |total, n| total + n }
+      assert_equal 15, result
+    end
+  end
 end
 
 class FizzBuzz
+  MAX_NUMBER = 100
+
   def self.generate(number)
     is_fizz = number.modulo(3).zero?
     is_buzz = number.modulo(5).zero?
@@ -72,6 +152,6 @@ class FizzBuzz
   end
 
   def self.generate_list
-    (1..100).map { |i| generate(i) }
+    (1..MAX_NUMBER).map { |i| generate(i) }
   end
 end
