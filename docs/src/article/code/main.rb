ff61e482291caf3076907e3d0438a283c569e13c
k2works <kakimomokuri@gmail.com>
2020-01-15 18:33:04 +0900

    docs: エピソード1

--- docs/src/article/code/main.rb
+++ docs/src/article/code/main.rb
@@ -32,128 +32,36 @@ class FizzBuzzTest < Minitest::Test
       end
     end
 
-    describe '1から100までの数を返す' do
-      def setup
-        @result = FizzBuzz.generate_list
+    describe '1から100までの数の配列を返す' do
+      def test_配列の初めは文字列の1を返す
+        result = FizzBuzz.print_1_to_100
+        assert_equal '1', result.first
       end
 
-      def test_はじめは文字列1を返す
-        assert_equal '1', @result.first
+      def test_配列の最後は文字列の100を返す
+        result = FizzBuzz.print_1_to_100
+        assert_equal '100', result.last
       end
-
-      def test_最後は文字列Buzzを返す
-        assert_equal 'Buzz', @result.last
-      end
-
-      def test_2番目は文字列Fizzを返す
-        assert_equal 'Fizz', @result[2]
-      end
-
-      def test_4番目は文字列Buzzを返す
-        assert_equal 'Buzz', @result[4]
-      end
-
-      def test_14番目は文字列FizzBuzzを返す
-        assert_equal 'FizzBuzz', @result[14]
-      end
-    end
-  end
-
-  describe '配列や繰り返し処理を理解する' do
-    def test_繰り返し処理
-      $stdout = StringIO.new
-      [1, 2, 3].each { |i| p i * i }
-      output = $stdout.string
-
-      assert_equal "1\n" + "4\n" + "9\n", output
-    end
-
-    def test_特定の条件を満たす要素だけを配列に入れて返す
-      result = [1.1, 2, 3.3, 4].select(&:integer?)
-      assert_equal [2, 4], result
-    end
-
-    def test_特定の条件を満たす要素だけを配列に入れて返す
-      result = [1.1, 2, 3.3, 4].find_all(&:integer?)
-      assert_equal [2, 4], result
-    end
-
-    def test_特定の条件を満たさない要素だけを配列に入れて返す
-      result = [1.1, 2, 3.3, 4].reject(&:integer?)
-      assert_equal [1.1, 3.3], result
-    end
-
-    def test_新しい要素の配列を返す
-      result = %w[apple orange pineapple strawberry].map(&:size)
-      assert_equal [5, 6, 9, 10], result
-    end
-
-    def test_新しい要素の配列を返す
-      result = %w[apple orange pineapple strawberry].collect(&:size)
-      assert_equal [5, 6, 9, 10], result
-    end
-
-    def test_配列の中から条件に一致する要素を取得する
-      result = %w[apple orange pineapple strawberry].find(&:size)
-      assert_equal 'apple', result
-    end
-
-    def test_配列の中から条件に一致する要素を取得する
-      result = %w[apple orange pineapple strawberry].detect(&:size)
-      assert_equal 'apple', result
-    end
-
-    def test_指定した評価式で並び変えた配列を返す
-      assert_equal %w[1 10 13 2 3 4], %w[2 4 13 3 1 10].sort
-      assert_equal %w[1 2 3 4 10 13],
-                   %w[2 4 13 3 1 10].sort { |a, b| a.to_i <=> b.to_i }
-      assert_equal %w[13 10 4 3 2 1],
-                   %w[2 4 13 3 1 10].sort { |b, a| a.to_i <=> b.to_i }
-    end
-
-    def test_配列の中から、条件に一致する要素を取得する
-      result = %w[apple orange pineapple strawberry apricot].grep(/^a/)
-      assert_equal %w[apple apricot], result
-    end
-
-    def test_ブロック内の条件式が真である間までの要素を返す
-      result = [1, 2, 3, 4, 5, 6, 7, 8, 9].take_while { |item| item < 6 }
-      assert_equal [1, 2, 3, 4, 5], result
-    end
-
-    def test_ブロック内の条件式が真である以降の要素を返す
-      result = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].drop_while { |item| item < 6 }
-      assert_equal [6, 7, 8, 9, 10], result
-    end
-
-    def test_畳み込み演算を行う
-      result = [1, 2, 3, 4, 5].inject(0) { |total, n| total + n }
-      assert_equal 15, result
-    end
-
-    def test_畳み込み演算を行う
-      result = [1, 2, 3, 4, 5].reduce { |total, n| total + n }
-      assert_equal 15, result
     end
   end
 end
 
 class FizzBuzz
-  MAX_NUMBER = 100
-
   def self.generate(number)
-    is_fizz = number.modulo(3).zero?
-    is_buzz = number.modulo(5).zero?
-
-    return 'FizzBuzz' if is_fizz && is_buzz
-    return 'Fizz' if is_fizz
-    return 'Buzz' if is_buzz
-    number.to_s
+    result = number.to_s
+    if number.modulo(3).zero? && number.modulo(5).zero?
+      result = 'FizzBuzz'
+    elsif number.modulo(3).zero?
+      result = 'Fizz'
+    elsif number.modulo(5).zero?
+      result = 'Buzz'
+    end
+    result
   end
 
-  def self.generate_list
-    (1..MAX_NUMBER).map { |i| generate(i) }
+  def self.print_1_to_100
+    result = []
+    (1..100).each { |n| result << n.to_s }
+    result
   end
 end
-
-puts FizzBuzz.generate_list
