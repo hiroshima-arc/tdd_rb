7a3962142600acd12d747d8cc7e655402fd5866e
k2works <kakimomokuri@gmail.com>
2020-01-25 18:28:49 +0900

    chore: コードカバレッジセットアップ

--- test/fizz_buzz_test.rb
+++ test/fizz_buzz_test.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
-
+require 'simplecov'
+SimpleCov.start
 require 'minitest/reporters'
 Minitest::Reporters.use!
 require 'minitest/autorun'
@@ -111,11 +112,11 @@ class FizzBuzzTest < Minitest::Test
     end
 
     def test_指定した評価式で並び変えた配列を返す
-      result1 = %w[1 10 13 2 3 4], %w[2 4 13 3 1 10].sort
+      result1 = %w[2 4 13 3 1 10].sort
       result2 = %w[2 4 13 3 1 10].sort { |a, b| a.to_i <=> b.to_i }
       result3 = %w[2 4 13 3 1 10].sort { |b, a| a.to_i <=> b.to_i }
 
-      assert_equal result1
+      assert_equal %w[1 10 13 2 3 4], result1
       assert_equal %w[1 2 3 4 10 13], result2
       assert_equal %w[13 10 4 3 2 1], result3
     end
