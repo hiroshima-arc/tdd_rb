5d1671d2ac606dc0c83971b726025df76123b028
k2works <k2works@gmail.com>
2020-02-08 12:54:02 +0900

    refactor: フィールドのカプセル化

--- test/fizz_buzz_test.rb
+++ test/fizz_buzz_test.rb
@@ -11,7 +11,7 @@ class FizzBuzzTest < Minitest::Test
   describe '数を文字列にして返す' do
     describe 'タイプ1の場合' do
       def setup
-        @fizzbuzz = FizzBuzz.new
+        @fizzbuzz = FizzBuzz.new(1)
       end
 
       describe '三の倍数の場合' do
@@ -40,7 +40,7 @@ class FizzBuzzTest < Minitest::Test
 
       describe '1から100までのFizzBuzzの配列を返す' do
         def setup
-          fizzbuzz = FizzBuzz.new
+          fizzbuzz = FizzBuzz.new(1)
           fizzbuzz.generate_list
           @result = fizzbuzz.list
         end
@@ -69,72 +69,72 @@ class FizzBuzzTest < Minitest::Test
 
     describe 'タイプ2の場合' do
       def setup
-        @fizzbuzz = FizzBuzz.new
+        @fizzbuzz = FizzBuzz.new(2)
       end
 
       describe '三の倍数の場合' do
         def test_3を渡したら文字列3を返す
-          assert_equal '3', @fizzbuzz.generate(3, 2)
+          assert_equal '3', @fizzbuzz.generate(3)
         end
       end
 
       describe '五の倍数の場合' do
         def test_5を渡したら文字列5を返す
-          assert_equal '5', @fizzbuzz.generate(5, 2)
+          assert_equal '5', @fizzbuzz.generate(5)
         end
       end
 
       describe '三と五の倍数の場合' do
         def test_15を渡したら文字列15を返す
-          assert_equal '15', @fizzbuzz.generate(15, 2)
+          assert_equal '15', @fizzbuzz.generate(15)
         end
       end
 
       describe 'その他の場合' do
         def test_1を渡したら文字列1を返す
-          assert_equal '1', @fizzbuzz.generate(1, 2)
+          assert_equal '1', @fizzbuzz.generate(1)
         end
       end
     end
 
     describe 'タイプ3の場合' do
       def setup
-        @fizzbuzz = FizzBuzz.new
+        @fizzbuzz = FizzBuzz.new(3)
       end
 
       describe '三の倍数の場合' do
         def test_3を渡したら文字列3を返す
-          assert_equal '3', @fizzbuzz.generate(3, 3)
+          assert_equal '3', @fizzbuzz.generate(3)
         end
       end
 
       describe '五の倍数の場合' do
         def test_5を渡したら文字列5を返す
-          assert_equal '5', @fizzbuzz.generate(5, 3)
+          assert_equal '5', @fizzbuzz.generate(5)
         end
       end
 
       describe '三と五の倍数の場合' do
         def test_15を渡したら文字列FizzBuzzを返す
-          assert_equal 'FizzBuzz', @fizzbuzz.generate(15, 3)
+          assert_equal 'FizzBuzz', @fizzbuzz.generate(15)
         end
       end
 
       describe 'その他の場合' do
         def test_1を渡したら文字列1を返す
-          assert_equal '1', @fizzbuzz.generate(1, 3)
+          assert_equal '1', @fizzbuzz.generate(1)
         end
       end
     end
 
     describe 'それ以外のタイプの場合' do
       def setup
-        @fizzbuzz = FizzBuzz.new
+        @fizzbuzz = FizzBuzz.new(4)
       end
 
       def test_例外を返す
         e = assert_raises RuntimeError do
-          @fizzbuzz.generate(1, 4)
+          @fizzbuzz.generate(1)
         end
 
         assert_equal '該当するタイプは存在しません', e.message
