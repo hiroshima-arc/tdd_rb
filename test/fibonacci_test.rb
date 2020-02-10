b01283160770265a12ad09cd32832f8a258492ab
k2works <kakimomokuri@gmail.com>
2020-02-10 17:03:13 +0900

    feat: フィボナッチ数計算

--- test/fibonacci_test.rb
+++ test/fibonacci_test.rb
@@ -10,18 +10,14 @@ class FibonacciTest < Minitest::Test
     @fib = Fibonacci
   end
 
-  def fib(number, memo = {})
-    Fibonacci.calc(number, memo)
-  end
-
   def test_fibonacci
     cases = [[0, 0], [1, 1], [2, 1], [3, 2], [4, 3], [5, 5]]
     cases.each do |i|
-      assert_equal i[1], fib(i[0])
+      assert_equal i[1], @fib.calc(i[0])
     end
   end
 
   def test_return_102334155_when_40
-    assert_equal 102_334_155, fib(40)
+    assert_equal 102_334_155, @fib.calc(40)
   end
 end
