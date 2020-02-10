5566f791ad0488d518f21628b5b28f82e68bcdac
k2works <kakimomokuri@gmail.com>
2020-02-10 06:32:53 +0000

    test: 明白な実装

--- test/fibonacci_test.rb
+++ test/fibonacci_test.rb
@@ -9,11 +9,11 @@ class Fibonacci < Minitest::Test
     return 0 if n.zero?
     return 1 if n <= 2
 
-    fib(n - 1) + 1
+    fib(n - 1) + fib(n - 2)
   end
 
   def test_fibonacci
-    cases = [[0, 0], [1, 1], [2, 1], [3, 2], [4, 3]]
+    cases = [[0, 0], [1, 1], [2, 1], [3, 2], [4, 3], [5, 5]]
     cases.each do |i|
       assert_equal i[1], fib(i[0])
     end
